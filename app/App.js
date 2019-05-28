df417e27543ef643a7fb2d10ce9c0b0e882623c8
Dominik <dominik_p@wp.pl>
2019-05-28 17:52:38 +0200

    added menu functionality

--- app/App.js
+++ app/App.js
@@ -1,6 +1,9 @@
 const cartOpenBtn = document.querySelector('#cart-open');
 const cart = document.querySelector('#cart');
 const closeCartBtn = document.querySelector('#close-cart');
+const menuOpenBtn = document.querySelector('#menu-open');
+const menu = document.querySelector('#menu');
+const closeMenuBtn = document.querySelector('#close-menu');
 
 export class App {
 
@@ -10,6 +13,13 @@ export class App {
             cart.classList.add('show-cart');
             closeCartBtn.addEventListener('click', e => {
                 cart.classList.remove('show-cart');
+            });
+        });
+
+        menuOpenBtn.addEventListener('click', e => {
+            menu.classList.add('show-menu');
+            closeMenuBtn.addEventListener('click', e => {
+                menu.classList.remove('show-menu');
             });
         });
     }
