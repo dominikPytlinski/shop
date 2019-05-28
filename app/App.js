8eea8d5beb6f2b3385bcfead8a042d5f0dbbbbfa
Dominik <dominik_p@wp.pl>
2019-05-28 17:40:46 +0200

    open and close cart functionality

--- app/App.js
+++ app/App.js
@@ -1,8 +1,17 @@
+const cartOpenBtn = document.querySelector('#cart-open');
+const cart = document.querySelector('#cart');
+const closeCartBtn = document.querySelector('#close-cart');
+
 export class App {
 
     constructor()
     {
-        
+        cartOpenBtn.addEventListener('click', e => {
+            cart.classList.add('show-cart');
+            closeCartBtn.addEventListener('click', e => {
+                cart.classList.remove('show-cart');
+            });
+        });
     }
 
 }
